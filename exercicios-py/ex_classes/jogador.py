b16ccecb8eb8b1920054f2303446dcd8c72ac0cd
Arthur Lola Oliveira <arthurlolaoliveira@hotmail.com>
2023-10-31 10:23:13 -0300

    criado metodo construtor

---
+++ exercicios-py/ex_classes/jogador.py
@@ -0,0 +1,9 @@
+class Jogador:
+
+    def __init__(self, nome, posicao, nascimento, nacionalidade, altura, peso):
+        self.nome = nome
+        self.posicao = posicao
+        self.nascimento = nascimento
+        self.nacionalidade = nacionalidade
+        self.altura = altura
+        self.peso = peso
