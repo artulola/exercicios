e872d793e0132686318d743496da5e38c33425f9
Arthur Lola Oliveira <arthurlolaoliveira@hotmail.com>
2023-11-01 10:10:53 -0300

    Atualizando exercicio

--- exercicios-py/ex_classes/jogador.py
+++ exercicios-py/ex_classes/jogador.py
@@ -7,3 +7,24 @@ class Jogador:
         self.nacionalidade = nacionalidade
         self.altura = altura
         self.peso = peso
+
+    def imprimirDados(self):
+        print("INFORMAÇÕES JOGADOR")
+        print(f'Nome: {self.nome}\nPosição: {self.posicao}\nAno de nascimento: {self.nascimento}\nNacionalidade: {self.nacionalidade}\nAltura: {self.altura}Peso: {self.peso}')
+
+    def calcularIdade(self):
+        idade = 2023 - self.nascimento
+        print(f'O jogador possui aproximadamente {idade} anos')
+
+    def tempoParaAposentar(self):
+        idade = 2023 - self.nascimento
+        if idade <= 40:
+            if self.posicao == 'def':
+                print(f'Faltam, aproximadamente, {40 - idade} anos para o jogador aposentar.')
+            elif self.posicao == 'mei':
+                print(f'Faltam, aproximadamente, {38 - idade} anos para o jogador aposentar.')
+            elif self.posicao == 'ata':
+                print(f'Faltam, aproximadamente, {35 - idade} anos para o jogador aposentar.')
+        else:
+            print('O jogador vai aposentar, ou, provavelmente já está aposentado.')
+            
---
+++ exercicios-py/ex_classes/mainjogador.py
@@ -0,0 +1,3 @@
+from ex_classes import Jogador
+
+jogador1 = Jogador()
